5676a88784134f1a6b91a7f11e0f83ccf1b99f50
zlampreht <zala.lampreht@gmail.com>
2025-07-08 10:11:56 +0200

    update

--- api/main.js
+++ api/main.js
@@ -1,12 +1,14 @@
-const express = require ('express');
+import express from 'express';
+import cors from 'cors';
+
 const app =  express ();
 const PORT  = 3000;
 
 app.use  (express.json())
 
 app.get ("/api/v1/health"  ,(req, res) => {
-    res.json ( {
-        message :  'ok'
+    res.json ({
+        message: 'test17758632'
     });
 })
 
