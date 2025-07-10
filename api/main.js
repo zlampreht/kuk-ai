d5cde36d90988f4ae62e64082a4b3fcd600db915
zlampreht <zala.lampreht@gmail.com>
2025-07-10 13:59:58 +0200

    imported generateRecipe

--- api/main.js
+++ api/main.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { register,login } from './views/auth.js';
 import { saveRecipe, getMyRecipes} from './views/recipes.js';
 import { jwtMiddleware } from './util.js';
+import { generateRecipe } from './views/ai.js';
 
 const app =  express ();
 const PORT  = 3000;
@@ -22,6 +23,7 @@ app.post("/api/v1/auth/login", login);
 app.post("/api/v1/recipes", jwtMiddleware, saveRecipe);
 app.get("/api/v1/recipes", jwtMiddleware, getMyRecipes)
 
+app.post("/api/v1/ai/generate-recipe", generateRecipe)
 
 app.listen(PORT, ()=> {
     console.log("app is running")
